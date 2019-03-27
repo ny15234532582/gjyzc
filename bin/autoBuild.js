cdbc3810e8087e37f5168cbcbfbee08506be10f5
ny15234532582 <1169831996@qq.com>
2019-03-27 18:46:10 +0800

    修改了autoBuild.js为git fetch --all && git reset --hard gjyzc/master && git pull

--- bin/autoBuild.js
+++ bin/autoBuild.js
@@ -7,11 +7,9 @@ WEB_USERGROUP='root'
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard gjyzc/master
-git clean -f
-git pull
-git checkout master
+git fetch --all && git reset --hard gjyzc/master && git pull
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
-npm run forever-restart
+npm run forever-stop
+npm run forever-start
 echo "Finished."
