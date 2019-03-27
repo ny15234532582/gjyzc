4b9732a93e97d791c28334df0f76fbf49ee28ce0
ny15234532582 <1169831996@qq.com>
2019-03-27 19:55:52 +0800

    1231

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ let server = app.listen(configs.serverPort,
     function(){
     tools.msg({
         type:'info',
-        msg:'启动服务器,地址:http://'+tools.localIP()+
+        msg:'启动服务器1,地址:http://'+tools.localIP()+
             ':'+configs.serverPort
     });
 });
